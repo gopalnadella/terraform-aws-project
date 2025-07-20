4bb33f09f2303edaff4ea40a169a559c9d8f6367
gopalnadell <gopalnadell@gmail.com>
2025-07-20 22:51:58 +0530

    added new deployment.yml file

--- backend-setup/s3-dynamodb.tf
+++ backend-setup/s3-dynamodb.tf
@@ -1,12 +1,32 @@
-terraform {
-  backend "s3" {
-    bucket         = "gk-state-file-cicd"
-    key            = "dev/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "users_dev-cicd"
-    encrypt        = true
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "gk-state-file-cicd-bucket"  # Change to a unique name
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags = {
+    Name        = "Terraform State Bucket"
+    Environment = "bootstrap"
   }
 }
-provider "aws" {
-  region = "us-east-1"
+
+resource "aws_dynamodb_table" "tf_lock" {
+  name         = "gk-terraform-locks-ddb"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "Terraform Lock Table"
+    Environment = "bootstrap"
+  }
 }
