27fb310741b20f16b7eb02e34958c87814e41184
gopalnadell <gopalnadell@gmail.com>
2025-07-20 22:30:02 +0530

    updated backend file

--- backend-setup/s3-dynamodb.tf
+++ backend-setup/s3-dynamodb.tf
@@ -1,42 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket         = "gk-state-file-cicd"
+    key            = "dev/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "users_dev-cicd"
+    encrypt        = true
+  }
+}
 provider "aws" {
   region = "us-east-1"
-}
-
-resource "aws_s3_bucket" "tf_state" {
-  bucket = "gk-tf-state-bucket"
-
-  versioning {
-    enabled = true
-  }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-
-  lifecycle {
-    prevent_destroy = true
-  }
-
-  tags = {
-    Name = "Terraform State Bucket"
-  }
-}
-
-resource "aws_dynamodb_table" "tf_lock" {
-  name         = "gk-tf-lock-table"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  tags = {
-    Name = "Terraform Lock Table"
-  }
 }
